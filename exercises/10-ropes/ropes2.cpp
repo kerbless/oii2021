b9985d34c3cb38346d0c52b4737007d8b7232592
kerbless <bluburro7@gmail.com>
2021-05-15 09:23:44 +0200

    dropped ropes

--- exercises/10-ropes/ropes2.cpp
+++ exercises/10-ropes/ropes2.cpp
@@ -5,6 +5,24 @@ int main() {
     freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
     int ropes; cin >> ropes;
+    int parent[ropes], weight[ropes], maxParent = 0, maxWeight = 0, frontIndex = 0;
+    for (int i = 0; i < ropes; i++) {
+        cin >> parent[i];
+        if (parent[i] > maxParent) maxParent = parent[i];
+    }
+    for (int i = 0; i < ropes; i++) {
+        cin >> weight[i];
+        if (weight[i] > maxWeight && parent[i] == maxParent) {
+            maxWeight = weight[i];
+            frontIndex = i;
+        }
+    }
     
+    for (int i = 0; i < ropes; i++) {
+        
+    }
+    
+    
+
     return 0;
 }
