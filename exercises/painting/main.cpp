bd3b331549c1836729a1f827020ab3835c6cd3e5
kerbless <bluburro7@gmail.com>
2021-05-15 10:35:38 +0200

    started fractal painting

--- exercises/painting/main.cpp
+++ exercises/painting/main.cpp
@@ -1,11 +1,15 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+void solve(char x[][]) {
+
+}
 
 int main() {
     freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-    
-    
+    int N, K; cin >> N; cin >> K;
+    char x[N][N];
 
     return 0;
 }
