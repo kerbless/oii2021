6073fe3d479ae80a80ae97a32ec8371e32743170
kerbless <bluburro7@gmail.com>
2021-05-17 14:00:23 +0200

    reorg

---
+++ DemoTerritoriali2021/pesci/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+    int T; cin >> T;
+    for(int i = 0; i < T; i++) {
+        
+    }
+    return 0;
+}
