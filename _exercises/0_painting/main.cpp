0e29ed8b1043ea1bb62b922456b41c5b2219f05b
kerbless <bluburro7@gmail.com>
2021-06-22 12:18:54 +0200

    trying painting for 2h

--- _exercises/0_painting/main.cpp
+++ _exercises/0_painting/main.cpp
@@ -1,15 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(char x[][]) {
+vector<string> applyModel(vector<string> model, vector<string> in) {
 
 }
 
 int main() {
     freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-    int N, K; cin >> N; cin >> K;
-    char x[N][N];
 
+    // getting sizes
+    int N, K; cin >> N; cin >> K;
+
+    // get model
+    vector<string> model(N);
+    for(int i = 0; i < N; i++)
+        cin >> model[i];
+
+    // calculate size of result matrix
+    int NK = pow(N, K);
+
+    // initialize result matrix
+    cout << "\n";
+    vector<string> result(NK);
+    for(int i = 0; i < NK; i++)
+        for(int j = 0; j < NK; j++)
+            result[i][j] = '.';
+
+    // print result matrix
+    for(int i = 0; i < NK; i++) {
+        for(int j = 0; j < NK; j++)
+            cout << result[i][j] << " ";
+        cout << "\n";
+    }
     return 0;
 }
