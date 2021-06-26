c7686915043c649fa735fb0f23b306ffbca8837b
kerbless <bluburro7@gmail.com>
2021-06-26 11:15:30 +0200

    working on quasipal

--- _exercises/0_quasipal/main.cpp
+++ _exercises/0_quasipal/main.cpp
@@ -2,9 +2,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool is_quasipal(string data, int head, int tail) {
+    if (tail-head < 2) return true;
+    if (data[head] == data[tail] or data[head] == '0' or data[tail] == '0')
+        is_quasipal(data, head+1, tail-1);
+    else return false;
+}
+
+bool are_quasipal(vector<string> data, int row, int col) {
+	string column = "";
+    for (int i = 0; i < col; i++) {
+        for (int j = 0; j < row; i++) {
+            column += data[j][i];
+        }
+        cout << column;
+    }
+    return false;
+}
+
+void prin(vector<string> data, int row) {
+    cout << "\n";
+    for (int i = 0; i < row; i++)
+        cout << data[i] << "\n";
+}
+
+void perm(vector<string> data, int head, int row, int col) {
+    if (head == row - 1)
+        prin(data, row);
+    for (int i = head; i < row; i++) {
+        swap(data[head], data[i]);
+        perm(data, head+1, row, col);
+        swap(data[head], data[i]);
+    }
+}
+
 int main() {
     freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-    // NEED COMMUTATIONS TO PROCEED
+    int row, col;
+    cin >> row >> col;
+    vector<string> data(row);
+    for (int i = 0; i < row; i++)
+        cin >> data[i];
+    
+    cout << are_quasipal(data, row, col);
     return 0;
 }
--- _learning/permutations.cpp
+++ _learning/permutations.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 using namespace std;
-
-
-void prin(int x[], int l) {
-    cout << "\n";
-    for (int i = 0; i < l; i++) {
-        cout << x[i] << " ";
+ 
+void permutations(string str, int i, int n) {
+    // base condition
+    if (i == n - 1)
+    {
+        cout << str << endl;
+        return;
+    }
+ 
+    // process each character of the remaining string
+    for (int j = i; j < n; j++)
+    {
+        // swap character at index `i` with the current character
+        swap(str[i], str[j]);        // STL `swap()` used
+ 
+        // recur for substring `str[i+1, n-1]`
+        permutations(str, i + 1, n);
+ 
+        // backtrack (restore the string to its original state)
+        swap(str[i], str[j]);
     }
 }
-
-int main() {
-    int array[] = {a, b, c};
-    
+ 
+int main()
+{
+    string str = "ABC";
+ 
+    permutations(str, 0, str.length());
+ 
+    return 0;
 }
