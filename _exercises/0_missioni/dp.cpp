9c5b172601818c874c7dadf0511b1bcdec89a7f8
kerbless <bluburro7@gmail.com>
2021-07-08 10:12:56 +0200

    worked on missioni dp

--- _exercises/0_missioni/dp.cpp
+++ _exercises/0_missioni/dp.cpp
@@ -1,26 +1,41 @@
+//https://training.olinfo.it/#/task/missioni/submissions<Paste>
 #include <bits/stdc++.h>
 using namespace std;
-int d[101], s[101], x[366];
 
 int main() {
     ifstream in("input.txt");
     ofstream out("output.txt");
     int n; in >> n;
-    for (int i = 0; i < n; i++)
-        in >> d[i] >> s[i];
+    n++; //compensate for the manually added zeros at index 0 (meet by i = 1!)
 
-    for (int i = 0; i < n; i++)
-        out << d[i] << " " << s[i] << "\n";
-
-    for (int i=0; i<n; i++)
-        for (int j = d[i]-s[i]; j >= 0; j--)
-            if (x[j] + 1 > x[j+d[i]])
-                d[j+d[i]] = x[j]+1;
+    //d = days needed; s = day limit; r = days used; x = missions done
+    int d[n] = {0}, s[n] = {0}, r[n] = {0}, x[n] = {0}; 
     
-    int max = x[0];
-    for (int i=0; i<366; i++)
-        if (x[i] > max) max = x[i];
-    out << max;
-
+    for (int i = 1; i < n; i++)
+        in >> d[i] >> s[i];
+    
+    //the real stuff
+    int mx = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            out << i << " " << j << " ";
+            if (x[j] + 1 > x[i]) { //useful
+                out << "useful "; 
+                if (r[j] + d[i] <= s[i]) { //doable
+                    out << "doable "; 
+                    r[i] = r[j] + d[i];
+                    x[i] = x[j] + 1;
+                }
+            }
+            out << "\n";
+        } 
+        if (x[i] > mx) mx = x[i];
+    }
+    
+    //DEBUG
+    for (int i = 0; i < n; i++)
+        out << d[i] << " " << setw(2) << s[i] << " " << setw(2) << r[i] << " " << setw(2) << x[i] << "\n";
+    
+    out << mx;
     return 0;
  }
