5ef56c7ce783ff5461c4ac342ba73e8e27fe3b24
kerbless <bluburro7@gmail.com>
2021-07-08 10:57:43 +0200

    renamed missioni, started piano, aborted piano

---
+++ _exercises/0_piano/main.cpp
@@ -0,0 +1,13 @@
+//https://training.olinfo.it/#/task/luiss_piano/statement
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    //freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+    
+    
+    return 0;
+}
