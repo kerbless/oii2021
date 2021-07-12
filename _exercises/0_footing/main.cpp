c34d817077a03b957edf77fedde804ea503e2239
kerbless <bluburro7@gmail.com>
2021-07-12 11:36:41 +0200

    getting up

---
+++ _exercises/0_footing/main.cpp
@@ -0,0 +1,12 @@
+//https://training.olinfo.it/#/task/footing/statement
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    //freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+    
+    return 0;
+}
