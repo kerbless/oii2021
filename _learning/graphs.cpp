1bb11bd65b04beba5c01c32157d03847ffa40aa7
kerbless <bluburro7@gmail.com>
2021-07-14 14:31:34 +0200

    dcg and ucwg representation, fit data in dag?

--- _learning/graphs.cpp
+++ _learning/graphs.cpp
@@ -6,15 +6,30 @@ void printvect(vector<int> v) {
         cout << " " << v[i];
 }
 
-void printdag(vector<vector<int>> graph) {
+void printdug(vector<vector<int>> graph) {
     for (int i = 0; i < graph.size(); i++) {
         cout << i+1 << " -> "; printvect(graph[i]); cout << "\n";
     }
+    cout << "\n";
 }
 
 int main() {
     vector<vector<int>> dag(4); //graph
     dag[0] = {2,3,4};
     dag[1] = {4};
-    printdag(dag);
+    vector<vector<int>> dcg(7); //graph (using 0 padding indexes 1+)
+    dcg[1] = {2};
+    dcg[2] = {3};
+    dcg[3] = {4};
+    dcg[4] = {2, 6};
+    dcg[5] = {3};
+    printdug(dag);
+    printdug(dcg);
+    vector<pair<pair<int, int>, int>> ucwg(6);
+    ucwg[0] = pair<pair<int, int>, int>(pair<int, int>(1,2), 1);
+    for (int i = 0; i < ucwg.size(); i++) {
+        cout << i << " = " << 
+    }
+    
+
 }
