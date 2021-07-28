e414909cbba9e85b12543eac08b51678ea7c54fc
kerbless <bluburro7@gmail.com>
2021-07-28 10:48:11 +0200

    trying to use binary tree, no success

--- _exercises/0_alberi/main.cpp
+++ _exercises/0_alberi/main.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 void printVect(vector<int> v) {
     for(vector<int>::iterator it = v.begin(); it < v.end(); it++) {
         cout << *it << " ";
@@ -13,20 +14,7 @@ struct node {
     int data;
     node *r;
 };
-
-vector<node> addnode(vector<node> graph, int data, bool left) {
-    node adding;
-    adding.l = NULL;
-    adding.r = NULL;
-    adding.data = data;
-    if (graph.empty()) graph.push_back(adding);
-    else {
-        graph.push_back(adding);
-        if (left) graph[graph.size()-2].l = &graph.back(); //last element inside the vector gets address
-        else graph[graph.size()-2].r = &graph.back(); //right side
-    }
-    return graph;
-}
+vector<node> graph;
 
 void inorder(node* x) {
     if (x == NULL) return;
@@ -35,33 +23,24 @@ void inorder(node* x) {
     inorder(x->r);
 }
     
-
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int N; cin >> N;
     vector<int> pre(N), post(N);
-    vector<node> graph;
     for (int i = 0; i < N; i++) {
         cin >> pre[i];
-        node adding;
-        adding.l = adding.r = NULL;
-        adding.data = pre[i];
-        graph.push_back(adding);
-        if (!graph.empty()) {
-            graph[graph.size()-2].l = &graph.back(); //last element inside the vector gets address
-        }
+        node temp;
+        temp.r = temp.l = NULL;
+        temp.data = pre[i];
+        graph.push_back(temp);
+        if (i != 0) graph[graph.size()-2].l = &graph.back();
+        cout << &graph.back() << " <" << graph.back().data << "> " << graph[graph.size()-2].l << "\n";
     }
+    cout << "\n"; printVect(pre);
     for (int i = 0; i < N; i++)
         cin >> post[i];
-    cout << "\n";
-    printVect(pre);
-    cout << "\n";
-    printVect(post);
-    cout << "\n";
-    for (int i = 0; i < graph.size(); i++) {
-        cout << graph[i].data << " " << &graph[i] << " " << graph[i].l << " " << graph[i].r << "\n";
-    }
-    inorder(&graph[graph.size()-5]);
+    cout << "\n"; printVect(post);
+    cout << "\n"; inorder(&graph.front());
     return 0;
 }
