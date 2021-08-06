47b4845b5afe8ad2a5542f546ffdf6b6100a0077
kerbless <bluburro7@gmail.com>
2021-08-06 17:42:49 +0200

    Merge branch 'main' of https://github.com/kerbless/oii2021 into main

---
+++ _learning/topologicalSorting.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<vector<int> > graph = {
+        {2},
+        {3, 4, 5},
+        {4},
+        {},
+        {4},
+        {},
+        }; 
+
+}
