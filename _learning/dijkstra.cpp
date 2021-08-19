6800b6da8284e138efdc5d3e2293477b0c3b5e15
kerbless <bluburro7@gmail.com>
2021-08-19 11:04:47 +0200

    started working on dijkstra

--- _learning/dijkstra.cpp
+++ _learning/dijkstra.cpp
@@ -1,14 +1,36 @@
 // GOAL: find shortest path from (1) to F (7) of the graph at slide 70
 #include <iostream>
+#include <queue>
 #include <limits>
 #include "UWG.cpp" // utility functions for Undirected Weighted Graphs made by me
 using namespace std;
 
-const unsigned int INF = numeric_limits<unsigned int>::max(); // now "inf" can simulate infinity because it's the max int representable 
+const int INF = numeric_limits<int>::max(); // now "inf" can simulate infinity because it's the max int representable 
 
 void dijkstra (vector<node> graph[], int vertices, int source, int target) {
-    T
-    for (int i = 0; i < vertices; i++) graph[i]
+    source--; target--; // id to index conversion
+    vector<int> dist(vertices, INF); 
+    dist[source] = 0;
+    for (int i = 0; i < vertices; i++) {
+        cout << "\ndistance s -> " << i+1 << " is " << dist[i];
+    }
+    priority_queue<pair<int, int>> pq;
+    pq.push(make_pair(0, source));
+    while (!pq.empty()) {
+        pair<int, int> looking = pq.top(); // looking = u
+        looking.first *= -1;
+        pq.pop();
+        for (auto& it : graph[looking.second]) { // v = it (u-v)
+            if (dist[it.id] = INF) {
+                pq.push(make_pair((dist[looking.first] + it.weight) * -1, it.id));
+                dist[it.id] = dist[looking] + it.weight;
+            }
+            else if (dist[looking] + it.weight < dist[it.id]) {
+                s.
+            }
+        }
+        break;
+    }
 }
 
 int main() {
@@ -28,3 +50,10 @@ int main() {
 
     return 0;
 }
+
+/*
+    for (int i = 0; i < vertices; i++) {
+        for (auto& it : graph[i]) {
+        }
+    }
+*/
